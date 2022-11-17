25fc12b57c8738087a37c0835eedeeca83c48658
Zanuar Bagus Ramadhan <zanuarbagusramadhan@gmail.com>
2022-11-17 20:27:57 +0700

    commit

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -10,4 +10,10 @@ const prefix = "/raz";
 // connection subrouter to mainrouter
 mainRouter.use(`${prefix}/auth`, authRouter);
 
+mainRouter.get("/welcome", (req, res) => {
+  res.json({
+    msg: "welcome in Raz Market",
+  });
+});
+
 module.exports = mainRouter;
