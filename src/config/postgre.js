dbe0b54e20b6ce0bc38595a74a84bdb089499753
Zanuar Bagus Ramadhan <zanuarbagusramadhan@gmail.com>
2022-11-17 11:41:35 +0700

    fixing

--- src/config/postgre.js
+++ src/config/postgre.js
@@ -1,10 +1,16 @@
 const { Pool } = require("pg");
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_DATABASE);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_PORT);
+console.log(process.env.PORT);
 const db = new Pool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 module.exports = db;
